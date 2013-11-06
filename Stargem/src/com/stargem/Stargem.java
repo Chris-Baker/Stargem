dd5be1b7af32a23a01c581077f38ebbda51a9bcf
Chris-Baker <chris@cbaker.co.uk>
2013-11-06 20:48:40 +0000

    Setup LibGDX project
    Setup a standard LibGDX project using setup tool.

---
+++ Stargem/src/com/stargem/Stargem.java
@@ -0,0 +1,30 @@
+package com.stargem;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Stargem implements ApplicationListener {
+		
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
