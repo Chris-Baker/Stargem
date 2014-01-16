3526750036f4073e0a1668ba8dd5ee7c04961066
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 17:55:25 +0000

    Main game simulation
    The game simulation contains a set of SubSystems which control the game.

---
+++ Stargem/src/com/stargem/models/Model.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.stargem.models;
+
+/**
+ * Model.java
+ *
+ * @author 	Chris B
+ * @date	19 Nov 2013
+ * @version	1.0
+ */
+public interface Model {
+
+	public void update(float delta);
+	
+}
---
+++ Stargem/src/com/stargem/models/Simulation.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.stargem.models;
+
+/**
+ * Simulation.java
+ *
+ * @author 	Chris B
+ * @date	19 Nov 2013
+ * @version	1.0
+ */
+public class Simulation implements Model {
+
+	
+	public Simulation() {
+		
+		// create all systems
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.stargem.models.Model#update(float)
+	 */
+	@Override
+	public void update(float delta) {
+		
+		// update physics
+		
+		// get list of entities in the local player's zone of control
+		
+		// update all systems
+		
+	}
+	
+}
