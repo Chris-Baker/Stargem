b8d24a7a652d8339ade4e49aab76395f6edc6843
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 18:27:20 +0000

    Interface to Observe Entity Recycle events
    Interface to Observe Entity Recycle events

---
+++ Stargem/src/com/stargem/entity/EntityRecycleObserver.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.stargem.entity;
+
+
+/**
+ * EntityRecycleObserver.java
+ *
+ * @author 	Chris B
+ * @date	12 Nov 2013
+ * @version	1.0
+ */
+public interface EntityRecycleObserver {
+
+	public void recycle(int entityId);
+		
+}
