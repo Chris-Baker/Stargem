cd143cd2213a01a77f2dce68b59778f384cc6207
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 18:10:42 +0000

    Config, Log, Preferences, and StringHelper
    Log is an abstraction of the libgdx logging classes
    Preferences contains various configurable game options such as controls
    Config contains constants
    StringHelper contains various string functions and a string builder
    object

---
+++ Stargem/src/com/stargem/Preferences.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.stargem;
+
+import com.badlogic.gdx.Input.Keys;
+
+/**
+ * Preferences.java
+ *
+ * @author 	Chris B
+ * @date	16 Dec 2013
+ * @version	1.0
+ */
+public class Preferences {
+
+	/* @formatter:off */
+	
+	// Keyboard input
+	public static int KEY_FORWARD 					= Keys.W;
+	public static int KEY_BACKWARD 					= Keys.S;
+	public static int KEY_LEFT 						= Keys.A;
+	public static int KEY_RIGHT 					= Keys.D;
+	public static int KEY_JUMP 						= Keys.SPACE;
+		
+	// Mouse Input
+	public static int MOUSE_SENSITIVITY 			= 10;
+		
+	/* @formatter:on */	
+}
---
+++ Stargem/src/com/stargem/StringHelper.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.stargem;
+
+/**
+ * StringHelper.java
+ *
+ * @author 	Chris B
+ * @date	6 Nov 2013
+ * @version	1.0
+ */
+public class StringHelper {
+
+	private static final StringBuilder builder = new StringBuilder();
+	
+	/**
+	 * Resets and returns the string builder object
+	 * @return the string builder object
+	 */
+	public static StringBuilder getBuilder() {
+		builder.setLength(0);
+		return builder;
+	}
+}
