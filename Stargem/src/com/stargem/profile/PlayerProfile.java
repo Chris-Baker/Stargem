11079cf31616b1c029b730f0871a329e4939a6c7
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 18:34:20 +0000

    PlayerProfile
    The PlayerProfile is managed by the ProfileManager. It contains
    information about the player's name, the current level and campaign, as
    well as the database name for the profile.

---
+++ Stargem/src/com/stargem/profile/PlayerProfile.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.stargem.profile;
+
+/**
+ * PlayerProfile.java
+ *
+ * @author 	Chris B
+ * @date	14 Nov 2013
+ * @version	1.0
+ */
+public class PlayerProfile {
+
+	protected String name;
+	protected String databaseName;
+	protected String campaignName;
+	protected String levelName;
+	
+	protected PlayerProfile() {}
+
+	/**
+	 * @return the campaignName
+	 */
+	public String getCampaignName() {
+		return campaignName;
+	}
+	
+	/**
+	 * @return the levelName
+	 */
+	public String getLevelName() {
+		return levelName;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the databaseName
+	 */
+	public String getDatabaseName() {
+		return databaseName;
+	}
+	
+}
