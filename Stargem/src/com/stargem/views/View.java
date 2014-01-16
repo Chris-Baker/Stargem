517b5e7652f65eca1d28999f7eaabb1775f13d4a
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 18:12:47 +0000

    View Interface
    A View has a render method.

---
+++ Stargem/src/com/stargem/views/View.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.stargem.views;
+
+/**
+ * View.java
+ *
+ * @author 	Chris B
+ * @date	20 Nov 2013
+ * @version	1.0
+ */
+public interface View {
+
+	public void render();
+	
+}
