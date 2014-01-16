913018f8a3ce28a515790471873bea5254538b2e
Chris-Baker <chris@cbaker.co.uk>
2014-01-16 18:31:25 +0000

    SimulationView
    The SimulationView implements the View interface and contains various
    Systems to display the game world.

---
+++ Stargem/src/com/stargem/views/SimulationView.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.stargem.views;
+
+/**
+ * SimulationView.java
+ *
+ * @author 	Chris B
+ * @date	20 Nov 2013
+ * @version	1.0
+ */
+public class SimulationView implements View {
+
+	/* (non-Javadoc)
+	 * @see com.stargem.views.View#render()
+	 */
+	@Override
+	public void render() {
+	}
+
+}
